1af931ecad22c0228936b6ad4da18ad1fe46fd3d
Petter Urkedal <urkedal@nbi.dk>
2014-01-06 15:19:42 +0100

    Update to new ocamlbuild-eliom.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -10,8 +10,6 @@ let server_packages = ["prime"; "prime.testing"; "config-file"]
 
 let setup_data = lazy (BaseEnvLight.load ())
 let setup_get k = BaseEnvLight.var_get k (Lazy.force setup_data)
-let test_params = Hashtbl.create 17
-let production_params = Hashtbl.create 17
 
 let rec catfilename = function
   | [] -> "." | [x] -> x
@@ -23,25 +21,12 @@ let () = dispatch begin function
     Options.use_ocamlfind := true;
     Options.make_links := false
 
-  | After_options ->
-    let localstatedir = setup_get "localstatedir" in
-    let staticdir = catfilename [setup_get "datadir"; project_name; "static"] in
-    let libdir = Filename.concat (setup_get "libdir") project_name in
-    set_ocsigen_params ~project_name ~packages:server_packages
-      ~libdir ~localstatedir ~staticdirs:[staticdir] production_params;
-    set_ocsigen_test_params ~project_name ~packages:server_packages
-      ~libdir:"_build/src/server" test_params
-
   | After_rules ->
     enable_eliom_rules ();
     Pathname.define_context "src/server" ["src"];
     Pathname.define_context "src/client" ["src"];
     flag ["ocaml"; "link"; "library"; "thread"] & A"-thread";
-
-    ocsigen_conf_rule ~create_dirs:true ~params:test_params
-                      ~prod:(project_name ^ "-test.conf")
-                      ~dep:(project_name ^ ".conf.in") ();
-    ocsigen_conf_rule production_params (project_name ^ ".conf") ()
+    enable_ocsigen_conf_rules ~server_packages ~server_subdir:"src/server" ()
 
   | e -> ()
 end
